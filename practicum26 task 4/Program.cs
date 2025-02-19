1136cab8ec9e7b84e197730d43c6d86fafc7cb53
Ghiorghi777 <goncargeorgij9@gmail.com>
2025-02-19 09:43:29 +0200

    Add files via upload

---
+++ practicum26 task 4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practicum26_task_4
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            // Явная инициализация массива
+            int[] array = { 3, 7, 1, 9, 12, 5, 8 };
+
+            // Ввод числа от пользователя
+            Console.Write("Введите число: ");
+            int number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Индексы элементов, больших {0}:", number);
+
+            // Поиск и вывод индексов элементов, больших заданного числа
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] > number)
+                {
+                    Console.WriteLine(i);
+                }
+                Console.Read();
+            }
+        }
+    }
+} 
