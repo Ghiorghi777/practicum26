e8b354ad6423a82e8d41b48280bf921aed2612c2
Ghiorghi777 <goncargeorgij9@gmail.com>
2025-02-19 09:43:02 +0200

    Add files via upload

---
+++ practicum26 task 3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practicum26_task_3
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int size = 10; // Размер массива
+            int[] array = new int[size];
+
+            // Заполнение массива индексами
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = i;
+            }
+
+            // Вывод массива с помощью foreach
+            foreach (int element in array)
+            {
+                Console.Write(element + " ");
+            }
+            Console.Read();
+        }
+    }
+
+}
+
